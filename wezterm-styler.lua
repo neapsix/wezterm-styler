be7c58d8d7dba40bbc52a41f45efccd48027063d
Benjamin Spiegel <bspiegel100@gmail.com>
2022-03-31 23:08:27 -0500

    Populate colors from built-in scheme or lua

--- wezterm-styler.lua
+++ wezterm-styler.lua
@@ -66,25 +66,59 @@ local function get_color_scheme_definitions(color_scheme, color_scheme_dirs)
     return color_scheme_definitions
 end
 
-local function override_colors(window)
-    -- Get the current colors
-    local colors_to_match = {}
-
+local function get_colors(window)
     local color_scheme = window:effective_config().color_scheme
-    local color_scheme_dirs = window:effective_config().color_scheme_dirs
 
     if color_scheme then
         -- Get the colors from the color scheme in config.color_scheme
-        colors_to_match = get_color_scheme_definitions(color_scheme, color_scheme_dirs)
+        local color_scheme_dirs = window:effective_config().color_scheme_dirs
+        return get_color_scheme_definitions(color_scheme, color_scheme_dirs)
     else
         -- Get the colors specified in lua in config.colors
-        colors_to_match = window:effective_config().colors
+        return window:effective_config().colors
     end
+end
 
-    local colors = {}
-    local window_frame = {}
-    -- TODO: Check whether we have the colors we need in the definitions (background, gray, selection, selection foregrounad)
-    -- TODO: Populate colors and window frame tables from the colors_to_match table
+local function build_colors(source)
+    local active_tab = {
+        bg_color = source['selection_bg'],
+        fg_color = source['selection_fg'],
+    }
+
+    local inactive_tab = {
+        bg_color = source['background'],
+        fg_color = source.brights[1], -- brights[0]: gray
+    }
+
+    return {
+        tab_bar = {
+            background = source['background'],
+            active_tab = active_tab,
+            inactive_tab = inactive_tab,
+            inactive_tab_hover = active_tab,
+            new_tab = inactive_tab,
+            new_tab_hover = active_tab,
+            inactive_tab_edge = source.brights[1], -- (Fancy tab bar only)
+        }
+    }
+end
+
+local function build_window_frame(source)
+    return {
+        active_titlebar_bg = source['background'],
+        inactive_titlebar_bg = source['background'],
+    }
+end
+
+local function override_colors(window)
+    -- Get the current colors
+    local colors_to_match = get_colors(window)
+
+    -- TODO: Check whether we have the colors we need in the definitions (background, gray, selection, selection foreground)
+
+    -- Populate colors and window frame tables from the colors_to_match table
+    local colors = build_colors(colors_to_match)
+    local window_frame = build_window_frame(colors_to_match)
 
     -- Get any config overrides that are already specified
     local overrides = window:get_config_overrides() or {}
@@ -103,12 +137,13 @@ end
 
 wezterm.on('window-config-reloaded', function(window, pane)
     -- debug_stuff(window)
-    local color_scheme = window:effective_config().color_scheme
+    --[[ local color_scheme = window:effective_config().color_scheme
     local color_scheme_dirs = window:effective_config().color_scheme_dirs
     if color_scheme then
         wezterm.log_info(get_color_scheme_path(color_scheme, color_scheme_dirs))
     else
         wezterm.log_info(window:effective_config().colors)
-    end
+    end ]]
+    override_colors(window)
 end)
 
